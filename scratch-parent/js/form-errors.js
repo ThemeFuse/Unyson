7d8e7ede90efab0933a80783d8ef0eb75c69e4cb
themefuse <hello@themefuse.com>
2014-10-06 18:22:47 +0300

    improved FW_Form errors display in frontend

--- scratch-parent/js/form-errors.js
+++ scratch-parent/js/form-errors.js
@@ -9,6 +9,8 @@
 			return;
 		}
 
+		var eventsNamespace = '.fw-form-errors';
+
 		$.each(localized.errors, function(name, error){
 			var $error = $('<div class="form-error"></div>');
 
@@ -16,12 +18,29 @@
 
 			var $input = $form.find('[name="'+ name +'"]');
 
-			if ($input.length) {
-				$error.insertAfter($input);
+			if (!$input.length) {
+				// maybe input name has array format, try to find by prefix: name[
+				$input = $form.find('[name^="'+ name +'["]');
+			}
 
-				$input.one('focus', function(){
-					$(this).next().slideUp(function(){ $(this).remove() });
-				});
+			if ($input.length) {
+				if ($input.length == 1) {
+					// there is only one input with the same name, attach error to it
+					$error.insertAfter($input);
+				} else {
+					// there are many inputs with the same name, attach error to parent container
+					$error.insertAfter( $input.first().closest('div') );
+				}
+
+				var errorId = ('form-error-'+ Math.random()).replace(/[^a-z0-9\-\_]/g, '');
+
+				$error.attr('id', errorId);
+
+				$input
+					.off('focus'+ eventsNamespace)
+					.one('focus'+ eventsNamespace, function(){
+						$('#'+ errorId).slideUp(function(){ $(this).remove() });
+					});
 			} else {
 				$form.prepend($error);
 			}
