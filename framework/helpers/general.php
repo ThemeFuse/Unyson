f50902753801abb43a0b2a66b84d734a69f2285f
David Ossa Hernandez <davidossahernandez@icloud.com>
2014-12-26 18:52:30 -0500

    Minor fix in the fw_current_url function

--- framework/helpers/general.php
+++ framework/helpers/general.php
@@ -785,10 +785,7 @@ function fw_current_url() {
 
 	$pageURL .= '://';
 
-	if ($_SERVER['SERVER_PORT'] != '80')
-		$pageURL .= $_SERVER['SERVER_NAME'].':'.$_SERVER['SERVER_PORT'].$_SERVER['REQUEST_URI'];
-	else
-		$pageURL .= $_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URI'];
+	$pageURL .= $_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
 
 	$cache = $pageURL;
 
