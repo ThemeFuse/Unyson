4c8cb94f9e0b38fb0929bb37cea9603f9f63d0dd
Valeriu <valeriuzdrobau@gmail.com>
2015-01-22 16:15:07 +0200

    fixed #180 This

--- framework/includes/option-types/slider/static/js/scripts.js
+++ framework/includes/option-types/slider/static/js/scripts.js
@@ -3,13 +3,10 @@
 		onChange: function (data) {
 			data.input.next('.fw-irs-range-slider-hidden-input').val(data.from + ';' + data.to);
 			data.input.closest('.fw-option-type-slider').find('span span.irs-slider.single').html(data.from);
-			data.input.closest('.fw-option-type-slider').find('.irs-bar').css('left', 0);
 		},
-		onStart: function(data) {
+		onStart: function (data) {
 			data.input.closest('.fw-option-type-slider').find('span span.irs-slider.single').html(data.from);
-
 			data.input.closest('.fw-option-type-slider').find('.irs-bar-edge').remove();
-			data.input.closest('.fw-option-type-slider').find('.irs-bar').css('left', '0%');
 		},
 		grid: true
 	};
@@ -18,11 +15,6 @@
 		data.$elements.find('.fw-option-type-slider').each(function () {
 			var options = JSON.parse($(this).attr('data-fw-irs-options'));
 			var slider = $(this).find('.fw-irs-range-slider').ionRangeSlider(_.defaults(options, defaults));
-
-			setTimeout(function(){
-				$(slider.context).find('.irs-bar-edge').remove();
-				$(slider.context).find('.irs-bar').css('left', '0%');
-			}, 0);
 		});
 	});
 
