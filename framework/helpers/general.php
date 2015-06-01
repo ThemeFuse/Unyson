b28f525514d92f9ae280a34e992665b027e7ff93
Alexei Gogu <llemurya@gmail.com>
2015-06-01 12:07:40 +0300

    add fw_get_image_sizes() function

--- framework/helpers/general.php
+++ framework/helpers/general.php
@@ -1221,3 +1221,53 @@ function fw_make_stylesheet_portable($href, $contents = null) {
 
 	return $contents;
 }
+
+/**
+ * Return all images sizes register by add_image_size() merged with
+ * WordPress default image sizes.
+ * @link https://codex.wordpress.org/Function_Reference/get_intermediate_image_sizes
+ * @param string $size
+ *
+ * @return array|bool
+ */
+function fw_get_image_sizes( $size = '' ) {
+
+	global $_wp_additional_image_sizes;
+
+	$sizes = array();
+	$get_intermediate_image_sizes = get_intermediate_image_sizes();
+
+	// Create the full array with sizes and crop info
+	foreach( $get_intermediate_image_sizes as $_size ) {
+
+		if ( in_array( $_size, array( 'thumbnail', 'medium', 'large' ) ) ) {
+
+			$sizes[ $_size ]['width'] = get_option( $_size . '_size_w' );
+			$sizes[ $_size ]['height'] = get_option( $_size . '_size_h' );
+			$sizes[ $_size ]['crop'] = (bool) get_option( $_size . '_crop' );
+
+		} elseif ( isset( $_wp_additional_image_sizes[ $_size ] ) ) {
+
+			$sizes[ $_size ] = array(
+				'width' => $_wp_additional_image_sizes[ $_size ]['width'],
+				'height' => $_wp_additional_image_sizes[ $_size ]['height'],
+				'crop' =>  $_wp_additional_image_sizes[ $_size ]['crop']
+			);
+
+		}
+
+	}
+
+	// Get only 1 size if found
+	if ( $size ) {
+
+		if( isset( $sizes[ $size ] ) ) {
+			return $sizes[ $size ];
+		} else {
+			return false;
+		}
+
+	}
+
+	return $sizes;
+}
