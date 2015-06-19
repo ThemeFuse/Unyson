bdac92b29b17478d15c97e2fdfd95e5dc7c21a90
moldcraft <moldcraft@email.com>
2015-06-19 18:55:22 +0300

    unit tests script fixes

--- tests/unit/run.bash
+++ tests/unit/run.bash
@@ -10,8 +10,17 @@ DB_PASS='root'
 DB_NAME='unyson_tests'
 WP_VERSION='latest'
 
-CWD=$(pwd)
 TMP_DIR='/tmp/unyson-test'
+WP_TESTS_DIR="$TMP_DIR/tests"
+
+# http://stackoverflow.com/a/246128
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+SCRIPT_DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 
 export WP_TESTS_DIR="$TMP_DIR/tests"
 
@@ -21,14 +30,12 @@ if test "`find $TMP_DIR -mmin +30`"; then
 fi
 
 if [ ! -d "$TMP_DIR" ]; then
-
 	mkdir -p "$TMP_DIR"
 	cd "$TMP_DIR"
 	wget https://raw.github.com/wp-cli/sample-plugin/master/bin/install-wp-tests.sh -O "$TMP_DIR/install-wp-tests.sh"
 	bash "$TMP_DIR/install-wp-tests.sh" $DB_NAME $DB_USER $DB_PASS $DB_HOST $WP_VERSION
-
 fi
 
 cd "$WP_TESTS_DIR"
 
-phpunit -c "$CWD/" --exclude-group invalid
+phpunit -c "$SCRIPT_DIR/" --exclude-group invalid
