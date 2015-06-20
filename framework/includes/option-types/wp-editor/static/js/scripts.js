b6c5e998e64bd19d9ef7a23ec5d405a5afd31e4e
moldcraft <moldcraft@email.com>
2015-06-20 12:59:51 +0300

    made backend-option fadeIn on js init

--- framework/includes/option-types/wp-editor/static/js/scripts.js
+++ framework/includes/option-types/wp-editor/static/js/scripts.js
@@ -121,7 +121,8 @@
 
 	fwe.on('fw:options:init', function(data) {
 		data.$elements
-			.find('.fw-option-type-wp-editor').each(init)
+			.find('.fw-option-type-wp-editor:not(.fw-option-initialized)')
+			.each(init)
 			.addClass('fw-option-initialized');
 	});
 
