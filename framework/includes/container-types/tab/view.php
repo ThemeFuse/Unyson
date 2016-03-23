8732591671c38ac4f214a04d37d55253e88ee961
moldcraft <moldcraft@email.com>
2016-03-23 18:06:20 +0200

    added 'li-attr' param for tabs fixes https://github.com/ThemeFuse/Unyson/issues/1379

--- framework/includes/container-types/tab/view.php
+++ framework/includes/container-types/tab/view.php
@@ -11,7 +11,10 @@ $lazy_tabs = fw()->theme->get_config('lazy_tabs');
 	<div class="fw-options-tabs-list">
 		<ul>
 			<?php foreach ($tabs as $tab_id => &$tab): ?>
-				<li><a href="#fw-options-tab-<?php echo esc_attr($tab_id) ?>" class="nav-tab fw-wp-link" ><?php echo htmlspecialchars($tab['title'], ENT_COMPAT, 'UTF-8') ?></a></li>
+				<li <?php echo isset($tab['li-attr']) ? fw_attr_to_html($tab['li-attr']) : ''; ?> >
+					<a href="#fw-options-tab-<?php echo esc_attr($tab_id) ?>" class="nav-tab fw-wp-link" ><?php
+						echo htmlspecialchars($tab['title'], ENT_COMPAT, 'UTF-8') ?></a>
+				</li>
 			<?php endforeach; unset($tab); ?>
 		</ul>
 		<div class="fw-clear"></div>
