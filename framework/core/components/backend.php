3f2f81c22d11e289390fcfcfbca6040ed2b115ea
moldcraft <moldcraft@email.com>
2016-11-23 15:16:37 +0200

    added link to issue

--- framework/core/components/backend.php
+++ framework/core/components/backend.php
@@ -455,6 +455,7 @@ final class _FW_Component_Backend {
 		/**
 		 * backwards compatibility, in case extensions are not up-to-date
 		 * todo: remove in next major version
+		 * https://github.com/ThemeFuse/Unyson/issues/2198
 		 * @deprecated
 		 */
 		wp_register_style('fw-font-awesome', fw_get_framework_directory_uri( '/static/libs/font-awesome/css/font-awesome.min.css' ));
