cee8a1592d4904fafd8d8d556b91bb69203459d5
Gheorghe <pisarenco.gh@gmail.com>
2016-12-02 16:35:58 +0200

    Removed the __constructor from FW_Option_Type (#2226)
    * Removed reference
    
    * Made FW_Option_Type access key as private member
    
    * No changes made. Minor code refactoring
    
    * Implemented autoload
    
    * Store options in FW_Cache
    
    * Added @since directive to exceptions
    
    * Removed the __constructor from FW_Option_Type

--- framework/core/exceptions/class-fw-option-type-exception.php
+++ framework/core/exceptions/class-fw-option-type-exception.php
@@ -2,18 +2,39 @@
 	die( 'Forbidden' );
 }
 
+
+/**
+ * Class FW_Option_Type_Exception
+ *
+ * @since 2.6.11
+ */
 class FW_Option_Type_Exception extends Exception {
 
 }
 
+/**
+ * Class FW_Option_Type_Exception_Not_Found
+ *
+ * @since 2.6.11
+ */
 class FW_Option_Type_Exception_Not_Found extends FW_Option_Type_Exception {
 
 }
 
+/**
+ * Class FW_Option_Type_Exception_Invalid_Class
+ *
+ * @since 2.6.11
+ */
 class FW_Option_Type_Exception_Invalid_Class extends FW_Option_Type_Exception {
 
 }
 
+/**
+ * Class FW_Option_Type_Exception_Already_Registered
+ *
+ * @since 2.6.11
+ */
 class FW_Option_Type_Exception_Already_Registered extends FW_Option_Type_Exception {
 
 }
