e0c3aa159ea52265a276b853ca7d6d6dfc82ff9f
Andrei Glingeanu <andrei.glingeanu@gmail.com>
2016-12-20 14:20:46 +0200

    introduce settings.disableResetButton

--- framework/static/js/fw.js
+++ framework/static/js/fw.js
@@ -1196,16 +1196,19 @@ fw.getValuesFromServer = function (data) {
 						text: _fw_localized.l10n.save,
 						priority: 40,
 						click: triggerSubmit
-					},
-					{
-						style: '',
-						text: _fw_localized.l10n.reset,
-						priority: -1,
-						click: function () {
-							modal.content.resetForm();
-						}
 					}
 				];
+
+			if (! settings.disableResetButton) {
+				buttons = buttons.concat([{
+					style: '',
+					text: _fw_localized.l10n.reset,
+					priority: -1,
+					click: function () {
+						modal.content.resetForm();
+					}
+				}]);
+			}
 
 			/**
 			 * Sometimes we want an apply button in order to save changes
