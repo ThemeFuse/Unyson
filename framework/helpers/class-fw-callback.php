9205f6dd9e71e2c358bf9a41d28b126de193fbb2
Cristeam <dev@cristeam.com>
2017-01-18 13:39:31 +0200

    Added fw_callback cache

--- framework/helpers/class-fw-callback.php
+++ framework/helpers/class-fw-callback.php
@@ -2,6 +2,11 @@
 	die( 'Forbidden' );
 }
 
+/**
+ * Class FW_Callback
+ *
+ * @since 2.6.14
+ */
 class FW_Callback {
 	/**
 	 * @var $callback string|array
@@ -13,20 +18,80 @@ class FW_Callback {
 	 */
 	private $args;
 
-	public function __construct( $callback, array $args = array() ) {
+	/**
+	 * @var bool
+	 */
+	private $cache;
+
+	/**
+	 * FW_Callback constructor.
+	 *
+	 * @param string|array $callback Callback function
+	 * @param array $args Callback arguments
+	 * @param bool $cache Whenever you want to cache the function value after it's first call or not
+	 * Recommend when the function call may require many resources or time (database requests) , or the value is small
+	 * Not recommended using on very large values
+	 *
+	 */
+	public function __construct( $callback, array $args = array(), $cache = true ) {
 		$this->callback = $callback;
 		$this->args     = $args;
+		$this->cache    = (bool) $cache;
 	}
 
+	/**
+	 * Return callback function
+	 * @return array|string
+	 */
 	public function get_callback() {
 		return $this->callback;
 	}
 
+	/**
+	 * Return callback function arguments
+	 * @return array
+	 */
 	public function get_args() {
 		return $this->args;
 	}
 
+	/**
+	 * Execute callback function
+	 * @return mixed
+	 */
 	public function execute() {
+		if ( $this->cache ) {
+			try {
+				return FW_Cache::get( $this->get_id() );
+			} catch ( FW_Cache_Not_Found_Exception $e ) {
+				$value = $this->get_value();
+				FW_Cache::set( $this->get_id(), $value );
+
+				return $value;
+			}
+		}
+
+		return $this->get_value();
+	}
+
+	/**
+	 * Whenever you want to clear the cached value or the function
+	 */
+	public function clear_cache() {
+		FW_Cache::del( $this->id );
+	}
+
+	/**
+	 * Get raw callback value, ignoring the cache
+	 * @return mixed
+	 */
+	protected function get_value() {
 		return call_user_func_array( $this->callback, $this->args );
+	}
+
+	protected function get_id() {
+		return 'fw-callback-' . md5( ! is_string( $this->callback )
+			? serialize( $this->callback )
+			: $this->callback );
 	}
 }
