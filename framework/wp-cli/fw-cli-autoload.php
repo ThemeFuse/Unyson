825e491be559dd54d2865deb6dfdbfdaf07279de
mowshon <mowshon@yahoo.com>
2017-02-14 16:15:11 +0200

    New CLI Command: Theme-Settings

--- framework/wp-cli/fw-cli-autoload.php
+++ framework/wp-cli/fw-cli-autoload.php
@@ -13,3 +13,4 @@ require __DIR__ . '/components/fw-cli-command.php';
 require_once __DIR__ . '/commands/fw-cli-command-unyson.php';
 require_once __DIR__ . '/commands/fw-cli-command-extensions.php';
 require_once __DIR__ . '/commands/fw-cli-command-backup.php';
+require_once __DIR__ . '/commands/fw-cli-command-theme-settings.php';
