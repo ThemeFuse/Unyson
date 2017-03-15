e31f84b1205d8cbeaceb62310a44322a1455efa9
Cristeam <dev@cristeam.com>
2017-03-15 09:49:32 +0200

    Fixed #2450

--- framework/helpers/general.php
+++ framework/helpers/general.php
@@ -1960,3 +1960,14 @@ function fw_call( $value ) {
 
 	return $value;
 }
+
+/**
+ * Check is the current value is instance of FW_Callback class
+ *
+ * @param mixed $value
+ *
+ * @return bool
+ */
+function fw_is_callback( $value ) {
+	return $value instanceof FW_Callback;
+}
