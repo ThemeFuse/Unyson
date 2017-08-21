9d0e5ec998a63aa0fb9569033ac2ea0948452bb2
sergiu-radu <rockablethemes@gmail.com>
2017-08-21 11:39:33 +0300

    Changes

--- framework/includes/option-types/icon-v2/views/templates.php
+++ framework/includes/option-types/icon-v2/views/templates.php
@@ -154,12 +154,21 @@ $tabs = fw()->backend->render_options(
 
 <# if (recent_uploads.length === 0) { #>
 
-	<h4>You have no uploaded icons.</h4>
+	<div class="fw-icon-v2-library-packs-wrapper">
+		<div class="fw-icon-v2-note">
 
-	<p>
-		You can simply click on the Upload button to upload more icons and
-		use them right away.
-	</p>
+			<svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 20 20">
+				<path d="M8 14V8H5l5-6 5 6h-3v6H8zm-2 2v-6H4v8h12.01v-8H14v6H6z"></path>
+			</svg>
+
+			<h3>You have no uploaded icons yet</h3>
+
+			<p>
+				You can simply click on the Upload button to upload more icons and
+				use them right away.
+			</p>
+		</div>
+	</div>
 
 <# } else { #>
 	
