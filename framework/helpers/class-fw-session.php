bf7e9d88100f79672daca1f8643ef2df71da32c8
ViorelEremia <eremiaviorel0@gmail.com>
2018-03-19 11:29:13 +0200

    Issue #2582, issue #3269 - server session disabled

--- framework/helpers/class-fw-session.php
+++ framework/helpers/class-fw-session.php
@@ -1,36 +1,54 @@
-<?php if (!defined('FW')) die('Forbidden');
+<?php if ( ! defined( 'FW' ) ) {
+	die( 'Forbidden' );
+}
 
 /**
  * Work with $_SESSION
  *
  * Advantages: Do not session_start() on every refresh, but only when it is accessed
  */
-class FW_Session
-{
-	private static function start_session()
-	{
-		if (!session_id()) {
+class FW_Session {
+	static $fw_session = array();
+
+	private static function start_session() {
+		if ( self::is_session() && ! session_id() ) {
 			session_start();
 		}
 	}
 
-	public static function get($key, $default_value = null)
-	{
+	public static function get( $key, $default_value = null ) {
 		self::start_session();
 
-		return fw_akg($key, $_SESSION, $default_value);
+		return fw_akg( $key, self::get_session_var(), $default_value );
 	}
 
-	public static function set($key, $value)
-	{
+	public static function set( $key, $value ) {
 		self::start_session();
 
-		fw_aks($key, $value, $_SESSION);
+		fw_aks( $key, $value, self::get_session_var() );
 	}
 
 	public static function del( $key ) {
 		self::start_session();
 
-		fw_aku( $key, $_SESSION );
+		fw_aku( $key, self::get_session_var() );
+	}
+
+	public static function is_session() {
+
+		if ( ! function_exists( 'session_status' ) || PHP_SESSION_DISABLED == session_status() || ! isset( $_SESSION ) ) {
+			return false;
+		}
+
+		return true;
+	}
+
+	public static function get_session_var() {
+
+		if ( self::is_session() ) {
+			return $_SESSION;
+		}
+
+		return self::$fw_session;
 	}
 }
